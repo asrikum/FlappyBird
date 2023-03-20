5d9e06cf8203a0278319d8da5669386c048256b5
asrikum <114449366+asrikum@users.noreply.github.com>
2023-03-20 17:44:58 -0400

    Update Player.cs

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -21,6 +21,14 @@ private void Start(){
 
     
 }
+
+private void OnEnable()
+{
+    Vector3 position = transform.position;
+    position.y =0f;
+    transform.position = position;
+    direction = Vector3.zero;
+}
    private void Update()
    {
 if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)){
@@ -42,4 +50,15 @@ transform.position += direction * Time.deltaTime;
     }
     SpriteRenderer.sprite = sprites[spriteIndex];
    }
+   private void OnTriggerEnter2D(Collider2D other)
+   {
+if (other.gameObject.tag == "Obstacle"){
+FindObjectOfType<GameManager>().GameOver();
 }
+else if (other.gameObject.tag== "Scoring"){
+    FindObjectOfType<GameManager>().IncreaseScore();
+}
+}
+   }
+
+
