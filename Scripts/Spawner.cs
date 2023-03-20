8ff3cf208b173b986221b543025c0d99f1700a54
asrikum <114449366+asrikum@users.noreply.github.com>
2023-03-20 17:45:35 -0400

    Update Spawner.cs

--- Scripts/Spawner.cs
+++ Scripts/Spawner.cs
@@ -12,18 +12,19 @@ public float minHeight = -1f;
 public float maxHeight = 1f;
 
 
-private void onEnable()
+private void OnEnable()
 {
     InvokeRepeating(nameof(Spawn), spawnRate, spawnRate);
 }
 
-private void OnDisable(){
+private void OnDisable()
+{
     CancelInvoke(nameof(Spawn));
 }
 
 private void Spawn()
 {
-GameObject pipes = Instantiate(prefab, transform.position, Quaternion.identity);
-pipes.transform.position += Vector3.up *  Random.Range(minHeight, maxHeight);
+    GameObject pipes = Instantiate(prefab, transform.position, Quaternion.identity);
+    pipes.transform.position += Vector3.up * Random.Range(minHeight, maxHeight);
 }
 }
